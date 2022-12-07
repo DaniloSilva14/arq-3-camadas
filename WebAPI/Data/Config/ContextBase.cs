93de96d3f54edef00fc4a9b2d1bba56e5fc9f8c0
Danilo Silva <danilo.silva@domvsit.com.br>
2022-12-06 23:51:40 -0300

    Projeto Inicial

---
+++ WebAPI/Data/Config/ContextBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Produto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Config
+{
+    public class ContextBase : DbContext
+    {
+
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql(GetStringConectionConfig(), new MySqlServerVersion(new Version(8, 0, 29)));
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        public DbSet<ProdutoViewModel> ProdutoViewModel { get; set; }
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "server=localhost;port=3320;database=api3camadas;user=user;password=pass";
+            return strCon;
+        }
+
+
+
+    }
+}
